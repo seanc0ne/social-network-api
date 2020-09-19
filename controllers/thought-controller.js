7e677cc248f3b1675f14d777e23978b6c024d3aa
Sean Cone <seandoesitbetter@gmail.com>
2020-09-18 22:36:21 -0700

    Update thought-controller.js

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -8,38 +8,43 @@ const thoughtController = {
           .catch(err => {
             console.log(err);
             res.sendStatus(400);
-          });
-      },
+        });
+    },
+
     // get one thought by id
-getThoughtById({ params }, res) {
-    Thought.findOne({ _id: params.id })
-      .select('-__v')
-      .then(dbThoughtData => res.json(dbThoughtData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  },
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
     // create thought and add it to a user - POST /api/thoughts/:userId
-  createThought({ params, body }, res) {
-    Thought.create(body)
-    .then(({ _id }) => {        // pass the _id of the newly created thought
-      return User.findOneAndUpdate(   // and update the array of thoughts of the associated user
-        { _id: params.userId },
-        { $push: { thoughts: _id } },
-        { new: true }   // b/c we want to return the updated user data
-      )
-      .select('-__v');
-    })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => res.json(err));
-  },
+    createThought({ body }, res) {
+        console.log(body)
+        Thought.create(body)
+            .then((dbThoughtData) => {        // pass the _id of the newly created thought
+                return User.findOneAndUpdate(   // and update the array of thoughts of the associated user
+                    { _id: body.userId },
+                    { $push: { thoughts: dbThoughtData._id } },
+                    { new: true }   // b/c we want to return the updated user data
+                )
+                    .select('-__v');
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                console.log(dbUserData)
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+    },
+
     // update Thought by id
     updateThought({ params, body }, res) {
         Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
@@ -52,6 +57,7 @@ getThoughtById({ params }, res) {
             })
             .catch(err => res.status(400).json(err));
     },
+    
     // delete Thought
     deleteThought({ params }, res) {
         Thought.findOneAndDelete({ _id: params.id })
