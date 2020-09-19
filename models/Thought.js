0f3e0cebae77df2e5b0435521e918bcd90938642
Sean Cone <seandoesitbetter@gmail.com>
2020-09-18 23:52:01 -0700

    Adding ReactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,35 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: "Enter a reaction.",
+      maxlength: [280, "Thought must be 280 characters or less."]
+    },
+    username: {
+      type: String,
+      required: "Username required."
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    }
+  }
+);
+
 const ThoughtSchema = new Schema(
     {
       thoughtText: {
@@ -16,8 +46,8 @@ const ThoughtSchema = new Schema(
      username: {
        type: String,
        required: 'Please enter your Username'
-     }
-    //  reactions: []
+     },
+     reactions: [ReactionSchema]
     },
     {
       toJSON: {
@@ -27,10 +57,11 @@ const ThoughtSchema = new Schema(
       id: false
     }
   );
-// create the User model using the UserSchema
+ 
 const Thought = model('Thought', ThoughtSchema);
-// ThoughtSchema.virtual('reactionsCount').get(function() {
-//   return this.reactions.length;
-// });
-// export the User model
+
+ThoughtSchema.virtual('reactionsCount').get(function() {
+  return this.reactions.length;
+});
+
 module.exports = Thought;
